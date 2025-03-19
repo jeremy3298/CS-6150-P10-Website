0a95db43a2f9a0eae13c89966488f62dffd24f00
harishmurali01 <103977173+harishmurali01@users.noreply.github.com>
2025-03-19 01:03:16 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,56 +10,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col p-4">
-      <h1 className="text-lg mb-4">Renewvia Solar Impact: Socio-Economic Insights</h1>
+      <h1 className="text-xl font-bold mb-4">Renewvia Solar Impact: Socio-Economic Insights</h1>
       <p>
         <a className="text-link cursor-pointer" href="#" onClick={handlePeerEvalClick}>
           Peer Evaluation Survey
         </a>
       </p>
-      <br />
-      <div className="w-full max-w-3xl text-left">
-        <p className="text-lg mt-4 text-left">Our changes to the Renewvia survey can be contextualized to a few main points:</p>
-        <ul className="list-disc ml-6">
-          <li>Strengthen the baseline data to better track pre-electrification income, energy, and socio-economic levels. </li>
-          <li>Isolate Impact by collecting data from a control group in neighborhoods with a similar demographic. </li>
-          <li>Adding new metrics from additional questions related to network connection, gender roles, education, and income in order to align further with Sustainable Development Goals (SDGs) set by the UN.</li>
-          <li>Altering existing questions to align with the SDGs and frameworks like the Multidimensional Energy Poverty Index. Furthermore, existing gaps in the survey have been fixed by removing biases in certain questions and altering them to gather more data (asking questions in a scaled or qualitative way rather than a binary way).</li>
-          <li>Changing the structure of the survey so it can be given out at certain intervals (6, 9, 12 months post-electrification) to track changes in metrics over time rather than a one-time impact.</li>
-          <li>Receive household feedback on mini-grid connection</li>
-        </ul>
-        <p className="text-lg mt-4 text-left">New Quantitative Measures for Socio-Economic Impact:</p>
-        <ul className="list-disc ml-6">
-          <li>Increase in Female Business Ownership</li>
-          <li>Growth in Internet-Connected Devices Per Household</li>
-          <li>Rise in Digital Banking Adoption</li>
-          <li>Increase in Remote Online Workers</li>
-          <li>Improvement in Female School Attendance</li>
-          <li>Growth in Total Business Ownership</li>
-          <li>Shift from Farming/Fishing/Home-Making to Other Occupations</li>
-          <li>Reduction in Number of Individuals Without Income</li>
-          <li>Change in Monthly Household Utility Costs</li>
-          <li>Improvement in Business Productivity</li>
-          <li>Increase in Electricity Satisfaction Levels</li>
-          <li>Reduction in Population Below the International Poverty Line</li>
-        </ul>
-        <p className="mt-4">
-          These new measurements are calculated from binary variables, linear scales, and other quantitative responses received from newly added questions.
-        </p>
-        <p className="mt-4">
-          View the current metrics here:&nbsp;
-          <a
-            className="text-link"
-            href="https://github.com/Renewvia-Energy/impact-analysis/tree/main/data"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Current Metrics
-          </a>
-        </p>
-      </div>
-      <br />
-      <hr className="w-full border-t border-gray-300 my-6" />
-      <p>
+        <p>
         This is the site for the P10 Renewvia Analysis Team! Take a look at the previous team's work&nbsp;
         <a
           className="text-link"
@@ -102,6 +59,50 @@ export default function Home() {
         </a>
         .
       </p>
+      <br />
+      <hr className="w-full border-t border-gray-300 my-6" />
+      <div className="w-full max-w-3xl text-left">
+        <p className="text-lg mt-4 text-left">Our changes to the Renewvia survey can be contextualized to a few main points:</p>
+        <ul className="list-disc ml-6">
+          <li>Strengthen the baseline data to better track pre-electrification income, energy, and socio-economic levels. </li>
+          <li>Isolate Impact by collecting data from a control group in neighborhoods with a similar demographic. </li>
+          <li>Adding new metrics from additional questions related to network connection, gender roles, education, and income in order to align further with Sustainable Development Goals (SDGs) set by the UN.</li>
+          <li>Altering existing questions to align with the SDGs and frameworks like the Multidimensional Energy Poverty Index. Furthermore, existing gaps in the survey have been fixed by removing biases in certain questions and altering them to gather more data (asking questions in a scaled or qualitative way rather than a binary way).</li>
+          <li>Changing the structure of the survey so it can be given out at certain intervals (6, 9, 12 months post-electrification) to track changes in metrics over time rather than a one-time impact.</li>
+          <li>Receive household feedback on mini-grid connection</li>
+        </ul>
+        <p className="text-lg mt-4 text-left">New Quantitative Measures for Socio-Economic Impact:</p>
+        <ul className="list-disc ml-6">
+          <li>Increase in Female Business Ownership</li>
+          <li>Growth in Internet-Connected Devices Per Household</li>
+          <li>Rise in Digital Banking Adoption</li>
+          <li>Increase in Remote Online Workers</li>
+          <li>Improvement in Female School Attendance</li>
+          <li>Growth in Total Business Ownership</li>
+          <li>Shift from Farming/Fishing/Home-Making to Other Occupations</li>
+          <li>Reduction in Number of Individuals Without Income</li>
+          <li>Change in Monthly Household Utility Costs</li>
+          <li>Improvement in Business Productivity</li>
+          <li>Increase in Electricity Satisfaction Levels</li>
+          <li>Reduction in Population Below the International Poverty Line</li>
+        </ul>
+        <p className="mt-4">
+          These new measurements are calculated from binary variables, linear scales, and other quantitative responses received from newly added questions.
+        </p>
+        <p className="mt-4">
+          View the current metrics here:&nbsp;
+          <a
+            className="text-link"
+            href="https://github.com/Renewvia-Energy/impact-analysis/tree/main/data"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Current Metrics
+          </a>
+        </p>
+      </div>
+      <br />
+    
     </div>
   );
 }
